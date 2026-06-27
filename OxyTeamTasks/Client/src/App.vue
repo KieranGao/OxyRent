47597089e8642867a1ef97aedb404b2eaf783b56
OxyTheCrack <Gazy0906@outlook.com>
2026-06-28 01:05:00 +0800

    Delete OxyTeamTasks directory

--- OxyTeamTasks/Client/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<script setup>
-import { useAppStore } from '@/stores/app'
-// Initialize the app store to apply theme on startup
-useAppStore()
-</script>
